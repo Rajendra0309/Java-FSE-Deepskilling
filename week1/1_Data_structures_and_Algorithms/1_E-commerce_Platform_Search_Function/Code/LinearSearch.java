afcd87e82fa851286a5800315e7a31d670571ce4
Rajendra Guttedar <103703747+Rajendra0309@users.noreply.github.com>
2025-07-05 11:32:30 +0530

    week 1 completed

---
+++ week1/1_Data_structures_and_Algorithms/1_E-commerce_Platform_Search_Function/Code/LinearSearch.java
@@ -0,0 +1,12 @@
+package com.week1.AlgorithmsDataStructures;
+
+public class LinearSearch {
+    public static Product linearSearch(Product[] products, String targetName) {
+        for (Product p : products) {
+            if (p.getProductName().equalsIgnoreCase(targetName)) {
+                return p;
+            }
+        }
+        return null;
+    }
+}
